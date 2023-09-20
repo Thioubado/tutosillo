e3a5da3be7969924d62b3d9cbd17cf6a3bd87ac3
Momo <seckndanane16@gmail.com>
2023-09-21 00:25:03 +0200

    Cours Laravel 10 – les bases – le routage

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('1', function () {
+    return 'Je suis la page 1 !';
+});
+
+Route::get('2', function () {
+    return 'Je suis la page 2 !';
+});
+
+Route::get('3', function () {
+    return 'Je suis la page 3 !';
+});
+
+// On peut utiliser un paramètre pour une route qui accepte des éléments variables en utilisant des accolades
+
+/* ce code accepte tous les parametres
+Route::get('{n}', function ($n) {
+    return 'Je suis la page ' .$n. ' !';
+});
+*/
+// parametre avec expression régulière
+Route::get('{n}', function ($n) {
+    return 'Je suis la page ' .$n. ' !';
+})->where('n', '[1-3]');
+
+// Route nommée
+Route::get('7', function () {
+    return "Je suis la page d'accueil";
+})->name('7');
