be459af46b14cb4922ed75a58f6abdd4a5cf1c15
GrCOTE7 <grcote7@gmail.com>
2024-05-31 00:56:51 +0200

    Fix: Register listener

---
+++ app/Listeners/AccueilListener.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * (ɔ) Mo & GrCOTE7 - 2022-2024
+ */
+
+namespace App\Listeners;
+
+use App\Events\Accueil as AcceuilEvent;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class AccueilListener
+{
+	/**
+	 * Create the event listener.
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Handle the event.
+	 */
+	public function handle(AcceuilEvent $event): void
+	{
+		DB::table('visits')->insert([
+			'ip'         => request()->ip(),
+			'created_at' => Carbon::now(),
+		]);
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
 use App\Events\Accueil;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     event(new Accueil);
